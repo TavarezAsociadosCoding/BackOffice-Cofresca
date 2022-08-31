3d1d7000f8be431c07a5aa333f61272dcc2b00e4
ltavarez <luis_aneuris16@gmail.com>
2022-08-31 01:52:09 -0400

    profile

--- src/app/components/invoice/invoice-routing.module.ts
+++ src/app/components/invoice/invoice-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
     path: '',
     component: InvoiceComponent,
     data: {
-      title: "Invoice",
-      breadcrumb: "Invoice"
+      title: "Facturas",
+      breadcrumb: "facturas"
     }
   },
 ];
---
+++ src/app/core/models/profile/profile.ts
@@ -0,0 +1,14 @@
+export interface Profile {
+  profileId: string;
+  userId: string;
+  userName: string;
+  email: string;
+  phone: string;
+  rnc: string;
+  address: string;
+  companyName: string;
+  phoneGerente: string;
+  isActive: boolean;
+  createdDate: Date;
+  aspNetUsers: null;
+}
